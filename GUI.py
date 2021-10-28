a941717992d91d0bf0729299aa1058a0d0b579bb
HarshZ26 <harshgsharma2002@gmail.com>
2021-10-28 22:39:11 +0530

    Done

--- GUI.py
+++ GUI.py
@@ -3,17 +3,17 @@ from tkinter import *
 from Modules import *
 root = Tk()
 
-root.geometry("1000x1000")
+root.geometry("800x600")
 root.title("Sign In")
 label=Label(root, text="Welcome",font="Algerian 30", fg="red")
-label.grid(row = 0,column =0)
+label.grid(row = 0,column =0, padx = (200,0))
 label2 = Label(root, text="to Hotel Taj",font="Algerian 30", fg="red")
 label2.grid(row = 0,column =1)
 user = Label(root, text="User Name",font="Algerian 16",anchor=W)
 pwd = Label(root, text="Password",font="Algerian 16",anchor=W)
 
-user.grid(row=1,column =0,padx=12,pady=20)
-pwd.grid(row=2,column =0,padx=12,pady=16)
+user.grid(row=1,column =0,padx=(300,5),pady=20)
+pwd.grid(row=2,column =0,padx=(300,5),pady=16)
 
 uservalue = StringVar()
 pwdvalue = StringVar()
@@ -25,6 +25,6 @@ userentry.grid(row=1,column=1,sticky = W)
 pwdentry.grid(row=2,column=1, sticky = W)
 
 bt1 =Button(text="Login",command = lambda:signin(uservalue.get(),pwdvalue.get(),root),padx=20,pady=10,font="20",bg="grey",fg="white")
-bt1.grid(row=3,column=1,sticky = SW)
+bt1.grid(row=3,column=1,padx=(0,80),pady=10)
 
 root.mainloop()
